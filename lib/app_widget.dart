1adafae15723f754e033aae30c7a87ffcb3d1226
gisele <gisesonia@gmail.com>
2023-06-07 22:16:33 -0300

    adicionando safe area e removendo debug banner

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -7,74 +7,77 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'School',
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Card(
-                  margin: EdgeInsets.all(0),
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      home: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Card(
+                    margin: EdgeInsets.all(0),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            child: Text('G'),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Gisele',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Spacer(),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.more_vert, color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
                     child: Row(
                       children: [
-                        CircleAvatar(
-                          child: Text('G'),
-                        ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Text(
-                          'Gisele',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Spacer(),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.more_vert, color: Colors.grey),
-                        )
+                        Text('Minhas estatísticas'),
                       ],
                     ),
                   ),
-                ),
-                const Divider(),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
+                  Row(children: [
+                    Icon(Icons.list, color: Colors.grey),
+                    Text('Total de notas: 0'),
+                  ]),
+                  Row(
                     children: [
-                      Text('Minhas estatísticas'),
+                      Icon(Icons.check, color: Colors.grey),
+                      Text('Concluídas: 0'),
                     ],
                   ),
-                ),
-                Row(children: [
-                  Icon(Icons.list, color: Colors.grey),
-                  Text('Total de notas: 0'),
-                ]),
-                Row(
-                  children: [
-                    Icon(Icons.check, color: Colors.grey),
-                    Text('Concluídas: 0'),
-                  ],
-                ),
-                const Divider(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Configurações'),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Tema escuro'),
-                    Switch(value: true, onChanged: (val) {}),
-                  ],
-                ),
-              ],
+                  const Divider(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Configurações'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Tema escuro'),
+                      Switch(value: true, onChanged: (val) {}),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
